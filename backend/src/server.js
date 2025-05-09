17ab0e9ec3f2d6ba88090b0d3bf555c471ba114c
Shivam Sharma <jtrshivamsharma@gmail.com>
2025-05-10 02:26:55 +0530

    feat: implement user authentication and onboarding flow
    - Added API functions for signup, login, logout, and fetching user data.
    - Created pages for Login, Sign Up, Onboarding, Home, Notifications, Chat, and Call functionalities.
    - Integrated Stream Chat and Video SDK for real-time communication.
    - Implemented friend request functionality with notifications.
    - Enhanced UI with Tailwind CSS and DaisyUI for better styling and responsiveness.
    - Introduced Zustand for theme management.
    - Added utility functions for common operations.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import "dotenv/config";
+import cors from 'cors';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import { connectDB } from './lib/db.js';
@@ -9,6 +10,10 @@ import chatRoutes from './routes/chat.route.js';
 const app = express();
 const PORT = process.env.PORT ;
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true, //allow frontend to send cookies
+}))
 app.use(express.json());
 app.use(cookieParser());
 
